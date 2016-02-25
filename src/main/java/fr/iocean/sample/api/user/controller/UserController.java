ea6edc936a694f67a897476e04cb223c92bc5680
Baptiste Le Bail <blebail@iocean.fr>
2016-02-25 17:15:37 +0100

    UserController hérite maintenant de UserDetailsController

--- src/main/java/fr/iocean/sample/api/user/controller/UserController.java
+++ src/main/java/fr/iocean/sample/api/user/controller/UserController.java
@@ -1,24 +1,21 @@
 package fr.iocean.sample.api.user.controller;
 
-import fr.iocean.framework.core.resource.controller.DeletableResourceController;
+import fr.iocean.framework.security.controller.UserDetailsController;
 import fr.iocean.sample.api.user.model.User;
 import fr.iocean.sample.api.user.service.UserService;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static fr.iocean.sample.SpringbokSampleApplication.API_ROOT_PATH;
 
-@Transactional
 @RestController
 @RequestMapping(value = API_ROOT_PATH + "/users")
-public class UserController extends DeletableResourceController<User, Long, UserService> {
-
+public class UserController extends UserDetailsController<User, Long, UserService> {
+    
     @PreAuthorize("hasRole('DELETE')")
     @Override
     public void deleteAll() {
         super.deleteAll();
     }
-    
 }
