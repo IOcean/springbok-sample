6b3b59c814a07eff394e84da307ca7705a03a257
Baptiste Le Bail <blebail@iocean.fr>
2016-04-08 16:26:21 +0200

    configuration

--- src/main/java/fr/iocean/sample/configuration/SecurityConfiguration.java
+++ src/main/java/fr/iocean/sample/configuration/SecurityConfiguration.java
@@ -1,14 +1,16 @@
 package fr.iocean.sample.configuration;
 
 import fr.iocean.framework.security.authentication.form.FormAuthentication;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
+@Slf4j
 @Configuration
 @Profile("!test")
 public class SecurityConfiguration extends FormAuthentication {
-
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
@@ -17,6 +19,4 @@ public class SecurityConfiguration extends FormAuthentication {
             .csrf()
             .disable();
     }
-
-    
 }
