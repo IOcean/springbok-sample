2094c2d3469639f34f78cad7599bd54c5ebcd6ed
Baptiste Le Bail <blebail@iocean.fr>
2016-04-11 18:37:20 +0200

    Http Basic auth

--- src/main/java/fr/iocean/sample/configuration/SecurityConfiguration.java
+++ src/main/java/fr/iocean/sample/configuration/SecurityConfiguration.java
@@ -1,22 +1,12 @@
 package fr.iocean.sample.configuration;
 
-import fr.iocean.framework.security.authentication.form.FormAuthentication;
+import fr.iocean.framework.security.authentication.http.HttpBasicAuthentication;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Slf4j
 @Configuration
 @Profile("!test")
-public class SecurityConfiguration extends FormAuthentication {
-    
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
-        
-        http
-            .csrf()
-            .disable();
-    }
+public class SecurityConfiguration extends HttpBasicAuthentication {
 }
--- src/test/java/fr/iocean/sample/configuration/SecurityTestConfiguration.java
+++ src/test/java/fr/iocean/sample/configuration/SecurityTestConfiguration.java
@@ -1,20 +1,10 @@
 package fr.iocean.sample.configuration;
 
-import fr.iocean.framework.security.authentication.form.FormAuthentication;
+import fr.iocean.framework.security.authentication.http.HttpBasicAuthentication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
 @Profile("test")
-public class SecurityTestConfiguration extends FormAuthentication {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
-        http
-            .csrf()
-            .disable();
-    }
-    
+public class SecurityTestConfiguration extends HttpBasicAuthentication {
 }
